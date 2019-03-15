f6ec2379a9a1ff429d55612c1d7a5bf942a89795
hoa151296 <hoa151296@gmail.com>
2019-03-15 13:15:37 +0700

    Hoa add model

---
+++ back-end/app/Models/Posts_Tags.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Posts_Tags extends Model
+{
+    protected $table = 'posts_tags';
+	public $timestamp = false;
+
+}
